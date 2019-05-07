86aeea147e01454cd0d4d8157d6905cdd449254b
rizkihalasan <rizki_halasan@yahoo.com>
2019-05-07 12:29:05 +0700

    pembersihan

--- src/dump/example purpose only/leaflet-example-pwa-glitch/workbox-cli-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globDirectory: './',
-  globPatterns: ['**/*.{html,png}', '/node_modules/leaflet/**/*.{css,js}'],
-  swDest: 'sw.js',
-  globIgnores: ['workbox-cli-config.js']
-}
